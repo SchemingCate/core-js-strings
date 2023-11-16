3c6de7ef2ce60839d7ae1db571ea55be35659f00
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-11-16 15:58:38 +0300

    feat: implement removeFirstOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -149,8 +149,12 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const indexOfValue = str.indexOf(value);
+  if (indexOfValue === -1) return str;
+  return (
+    str.substring(0, indexOfValue) + str.substring(indexOfValue + value.length)
+  );
 }
 
 /**
