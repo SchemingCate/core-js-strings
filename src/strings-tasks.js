6648a9f578dac1eabb53e2e62d5b19fe131637cd
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-11-16 17:37:07 +0300

    feat: implement invertCase

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -393,8 +393,15 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((l) =>
+      l.charCodeAt(0) > 96 && l.charCodeAt(0) < 123
+        ? l.toUpperCase()
+        : l.toLowerCase()
+    )
+    .join('');
 }
 
 /**
